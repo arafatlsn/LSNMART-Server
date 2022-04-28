52b42ae240d67851a90e72e5973dd413d24ac0f2
Arafat37 <arafatsarkar22@gmail.com>
2022-04-29 05:12:36 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 // middleware 
 app.use(cors());
@@ -13,15 +14,28 @@ app.get('/', (req, res) => {
 })
 
 // mongodb 
-function run(){
-  
-const uri = "mongodb+srv://<username>:<password>@cluster0.moy4n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.moy4n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('connected db')
-  client.close();
-});
+
+async function run(){
+
+try{
+
+  await client.connect()
+  const prodCollection = client.db('lsnmart').collection('products');
+
+  // load all products 
+  app.get('/products', async(req, res) => {
+    const query = {};
+    const cursor = prodCollection.find(query);
+    const result = await cursor.toArray();
+    res.send(result)
+  })
+
+}
+finally{
+
+}
 
 }
 run()
